acecfce5285c64605caab28b5f1d3e3044214df9
Pascal Heidmann <heidmann@kigaroo.de>
2020-09-01 10:08:15 +0200

    gnome extensions

--- setup.sh
+++ setup.sh
@@ -5,11 +5,17 @@ fi
 apt update -y
 apt upgrade -y
 apt-get install vim git docker docker-compose snapd chromium thunderbird gparted libreoffice
+
+if [ $(echo $XDG_CURRENT_DESKTOP | grep -i "gnome" | wc -l) -gt 0 ]; then
+    apt-get install gnome-shell-extensions;
+fi
+
 snap install --classic slack
 snap install spotify
 snap install postman
-[ ! -d ¨jetbrains-toolbox-1.17.7391¨ ] && wget -qO- "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.17.7391.tar.g" | tar xvz
+[ ! -d "jetbrains-toolbox-1.17.7391" ] && wget -qO- "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.17.7391.tar.g" | tar xvz
 groupadd docker
 usermod -aG docker $(whoami)
+newgrp docker
 systemctl enable docker
 
