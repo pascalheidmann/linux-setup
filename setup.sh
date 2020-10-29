31b70c48e7b8004a7dc15ba5cab9d647f7ce5f6e
Pascal Heidmann <heidmann@kigaroo.de>
2020-10-29 12:31:05 +0100

    jetbrains toolbox updated & fixed typo

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ fi
 snap install --classic slack
 snap install spotify
 snap install postman
-[ ! -d "jetbrains-toolbox-1.17.7391" ] && wget -qO- "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.17.7391.tar.g" | tar xvz
+[ ! -d "jetbrains-toolbox-1.18.7455" ] && wget -qO- "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7455.tar.gz" | tar xvz && echo "run ./jetbrains-toolbox-1.18.7455/jetbrains-toolbox to install toolbox"
 
 getent group docker || groupadd docker
 usermod -aG docker $(whoami)
