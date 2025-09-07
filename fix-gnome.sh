d4246cf2bd40237f55845f727a34cc15a2234795
Pascal Heidmann <pascalheidmann@users.noreply.github.com>
2025-09-07 18:35:43 +0200

    Update fix-gnome.sh

--- fix-gnome.sh
+++ fix-gnome.sh
@@ -1,3 +1,6 @@
 #/bin/bash
+sudo apt-get update -y
+sudo apt install gnome-shell-extension-manager
+
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
